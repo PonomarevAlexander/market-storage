7fd48467072279e7922586abc9463552a06808e8
Ponomarev <ponomarev.alex.1992@gmail.com>
2024-03-04 22:15:47 +0300

    initial master commit

---
+++ src/main/java/ru/ponomarev/marketstorage/MarketStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.ponomarev.marketstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketStorageApplication.class, args);
+    }
+
+}
